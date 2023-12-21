cbe584bd4602904882991f21b1ffbaeadc03b5a7
三山 <sanshan@ywwl.com>
2023-12-21 11:37:53 +0800

    feat: 添加页面标题

--- app/note/page.tsx
+++ app/note/page.tsx
@@ -1,5 +1,9 @@
 import { Editor } from "novel";
+import { Metadata } from 'next'
 
+export const metadata: Metadata = {
+  title: 'dashboard',
+}
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
