1d01af647ad5d34449d333922703aa1d87803985
三山 <sanshan@ywwl.com>
2024-03-21 10:25:02 +0800

    feat: 添加html转换插件

--- app/note/page.tsx
+++ app/note/page.tsx
@@ -1,13 +1,38 @@
+'use client'
 import { Editor } from "novel";
-import { Metadata } from 'next'
+import Bold from '@tiptap/extension-bold'
+// Option 2: Browser-only (lightweight)
+// import { generateHTML } from '@tiptap/core'
+import Document from '@tiptap/extension-document'
+import Paragraph from '@tiptap/extension-paragraph'
+import Text from '@tiptap/extension-text'
+// Option 1: Browser + server-side
+import { generateHTML, generateJSON } from '@tiptap/html'
+// 其他插件 https://github.com/ueberdosis/tiptap/tree/main/packages
 
-export const metadata: Metadata = {
-  title: 'dashboard',
-}
 export default function Home() {
+  const defaultHtmlStr = '<p>Example <strong>Text</strong></p>'
+  const defaultJson = generateJSON(defaultHtmlStr, [
+    Document,
+    Paragraph,
+    Text,
+    Bold,
+    // other extensions …
+  ])
+
+  const update = (editor) => {
+    const json = editor.getJSON();
+    console.log(generateHTML(json, [
+      Document,
+      Paragraph,
+      Text,
+      Bold,
+      // other extensions …
+    ]))
+  }
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <Editor />
+      <Editor onUpdate={update} defaultValue={defaultJson} disableLocalStorage/>
     </main>
   );
 }
